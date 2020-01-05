f30523f8b7df158d552a037c05eb6ea406f916e6
Sean Philippi <sean.philippi@gmail.com>
2020-01-05 16:25:47 -0600

    formatting date in seriesData

--- src/components/fetchCSV.js
+++ src/components/fetchCSV.js
@@ -22,6 +22,11 @@ const getCSVData = async () => {
   console.log('seriesNames', seriesNames)
   const seriesData = parsedData.slice(1, parsedData.length - 1)
   console.log('seriesData', seriesData)
+  // map over seriesData and format dates for later grouping with groupBy()
+  seriesData.forEach(data => data[9] = moment(data[9].split(' ')[0], 'YYYY-MM-DD').format('MMM DD YYYY'));
+  console.log('formattedSeriesData', seriesData)
+
+
   const seriesDates = seriesData.map((data, idx) => data[9].split(' ')[0])
   console.log('seriesDates', seriesDates)
 
@@ -32,11 +37,12 @@ const getCSVData = async () => {
       // series name
       name,
       // match series in each data point (row) using index and make array for Highcharts data
-      data: seriesData.map(row => Number(row[seriesIndex])),
-      date: moment(seriesData[seriesIndex][9].split(' ')[0], 'YYYY-MM-DD').format('MMM DD YYYY')
+      data: seriesData.map(row => Number(row[seriesIndex]))
     }
   })
   console.log('series', series)
+  const seriesByDate = _.groupBy(series, 'date');
+  console.log('seriesByDate', seriesByDate)
   return series;
 }
 
