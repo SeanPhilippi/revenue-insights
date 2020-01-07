6b7e97f6c489870942e3ba7cab2f16e63543dac9
Sean Philippi <sean.philippi@gmail.com>
2020-01-06 18:34:52 -0600

    working on filtering data.dates array

--- src/components/fetchCSV.js
+++ src/components/fetchCSV.js
@@ -27,8 +27,16 @@ const createSeries = (data, dateColIdx, valueColIdx, dateFormat) => {
   return groupedData;
 }
 
-
-const getCSVData = async () => {
+const getCSVData = async (currentChart) => {
+  // let dateColIdx = 9;
+  // let dateFormat = 'MMM DD YYYY';
+  // switch (currentChart) {
+  //   case '1m':
+  //     dateColIdx =
+  //   default:
+  //     dateColIdx = 9;
+  //     dateFormat = 'MMM DD YYYY';
+  // }
   const csvData = await fetchCSV();
   const parsedCsv = Papa.parse(csvData);
   const parsedData = parsedCsv.data;
@@ -45,8 +53,30 @@ const getCSVData = async () => {
     name: obj.name,
     data: obj.data.map(dataObj => dataObj.revenue)
   }));
-
-  const dates = seriesObjs[0].data.map(dataObj => dataObj.bin);
+  // ! filter dates based on currentChart value
+  // group by first 3 characters of each date string
+  let dates = seriesObjs[0].data.map(dataObj => dataObj.bin);
+  console.log('dates', dates)
+  if (currentChart === '1m') {
+    const month = dates[dates.length - 1].slice(0, 3);
+    dates = dates.filter(date => date.includes(month));
+    console.log('dates in if', dates)
+  } else if (currentChart === '6m') {
+    const months = dates.map(date => date.slice(0, 3));
+    const sixMonths = months.slice(-5);
+    dates = dates.filter(date => date.includes(sixMonths));
+  } else if (currentChart === 'ytd') {
+    const currentYear = dates[dates.length - 1].slice(-5);
+    console.log('currentYear', currentYear)
+    dates = dates.filter(date => date.includes(currentYear))
+    console.log('ytd dates', dates)
+  } else if (currentChart === '1y') {
+    const currentDate = moment().format('MMM DD YYYY');
+    console.log('currentDate', currentDate)
+    const furthestLimit = moment().subtract(12, 'months').format('MMM DD YYYY');
+    // const furthestLimit = currentDate.slice(0, -5) + ` ${Number(currentDate.slice(-5)) - 1}`;
+    console.log('furthestLimit', furthestLimit)
+  }
 
   return ({
     series,
@@ -54,6 +84,6 @@ const getCSVData = async () => {
   });
 }
 
-export default function() {
-  return getCSVData();
+export default function(currentChart) {
+  return getCSVData(currentChart);
 }
