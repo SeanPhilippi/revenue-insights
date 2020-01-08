3e313b9bee69d92bfee4118b7cf1608d87a75593
Sean Philippi <sean.philippi@gmail.com>
2020-01-07 21:00:21 -0600

    labels, quarters, years added

--- src/components/RevenueChart/DateFormatSelect.js
+++ src/components/RevenueChart/DateFormatSelect.js
@@ -10,24 +10,37 @@ const DateFormatSelect = () => {
     {
       name: 'MMM YYYY',
       value: 'months'
+    },
+    {
+      name: '[Q]Q YYYY',
+      value: 'quarters'
+    },
+    {
+      name: 'YYYY',
+      value: 'years'
     }
   ];
   return (
     <AppContext.Consumer>
       {
         ({ handleDateFormat, dateFormat }) => (
-          <div className="d-flex justify-content-center chart-select mb-2">
-            {dateFormats.map(format => (
-                <button
-                  name={ format.name }
-                  value={ format.value }
-                  className={ format.value === dateFormat.value && 'active-chart'}
-                  onClick={ handleDateFormat }
-                >
-                  { format.value }
-                </button>
-              )
-            )}
+          <div className="chart-select">
+            <label className="text-white">
+              Summarize By
+            </label>
+            <div className="d-flex justify-content-center mb-2">
+              {dateFormats.map(format => (
+                  <button
+                    name={ format.name }
+                    value={ format.value }
+                    className={ format.value === dateFormat.value && 'active-chart'}
+                    onClick={ handleDateFormat }
+                  >
+                    { format.value }
+                  </button>
+                )
+              )}
+            </div>
           </div>
         )
       }
